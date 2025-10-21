aeb1cdb4bbd38b6185e8921ff7e352a9bd2ac6bb
123456Eron <163343901+123456Eron@users.noreply.github.com>
2025-10-21 23:57:45 +0300

    Добавьте файлы проекта.

---
+++ LR1 Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MMKMFP_LR1_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите входные значения X1-X5 в столбик");
+            bool[] exis = new bool[5];
+            //Построчный ввод в массив входных значений.
+            for (int i = 0; i < exis.Length; i++) exis[i] = Convert.ToBoolean(Console.ReadLine());  
+            //Расчёт математической модели и вывод.
+            Console.WriteLine($"\r\nY = {(exis[0] && exis[1]) || (!exis[2] && exis[3]) || exis[4]}");  
+        }
+    }
+}
---
+++ MMKMFP LR1 Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MMKMFP_LR1_Task_2
+{
+    internal class Program
+    {
+        static bool IsValid(string s) => s.Length > 0 && s.Trim('0', '1').Length == 0;
+        static string Add(string a, string b)
+        {
+            // Остаток.
+            int c = 0;
+            var result = "";
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                // Считаем сумму с учётом переноса с учётом char формата.
+                int sum = a[i] - '0' + b[i] - '0' + c;
+                result = (sum % 2) + result;
+                c = sum / 2;
+            }
+            //Приписываем слева остаток.
+            return c == 1 ? "1" + result : result;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Введите в двоичном формате слагаемые одиноковой длины в столбик:");
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+            if (!IsValid(a) || !IsValid(b) || a.Length != b.Length)
+            {
+                Console.WriteLine("Ошибка ввода");
+                return;
+            }
+            Console.WriteLine(Add(a, b));
+        }
+    }
+}
