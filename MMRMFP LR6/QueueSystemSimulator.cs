bea197d976c5954742884f70e341eeca4cf52b32
123456Eron <163343901+123456Eron@users.noreply.github.com>
2025-12-16 09:52:26 +0300

    6 was fixed

--- MMRMFP LR6/QueueSystemSimulator.cs
+++ MMRMFP LR6/QueueSystemSimulator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace GasStationQueue
+﻿namespace GasStationQueue
 {
     public class QueueSystemSimulator
     {
--- MMRMFP LR6/RequestInfo.cs
+++ MMRMFP LR6/RequestInfo.cs
@@ -2,13 +2,35 @@
 {
     public class RequestInfo
     {
+        double arrivalTime, serviceStartTime, serviceEndTime;
         public int Id { get; set; }
 
-        public double ArrivalTime { get; set; }
+        public double ArrivalTime
+        { 
+            get => arrivalTime;
+            set 
+            {
+                arrivalTime = Math.Round(value, 1);
+            }
+        }
 
-        public double ServiceStartTime { get; set; }
+        public double ServiceStartTime
+        {
+            get => serviceStartTime;
+            set
+            {
+                serviceStartTime = Math.Round(value, 1);
+            }
+        }
 
-        public double ServiceEndTime { get; set; }
+        public double ServiceEndTime
+        {
+            get => serviceEndTime;
+            set
+            {
+                serviceEndTime = Math.Round(value, 1);
+            }
+        }
 
         public bool IsRejected { get; set; }
     }
